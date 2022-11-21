ca7616303c85e356958f9ec86481224d47447257
Mugiira KIm <mugiirakimathi@gmail.com>
2022-11-21 09:54:43 +0300

    ...

--- index.php
+++ index.php
@@ -28,59 +28,109 @@
    
 
     <link rel="stylesheet" href="style.css">
-    <style>
-      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+    <style type="text/css">
+.form-style-5{
+	max-width: 500px;
+	padding: 10px 20px;
+	background: #f4f7f8;
+	margin: 10px auto;
+	padding: 20px;
+	background: #f4f7f8;
+	border-radius: 8px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+}
+.form-style-5 fieldset{
+	border: none;
+}
+.form-style-5 legend {
+	font-size: 1.4em;
+	margin-bottom: 10px;
+}
+.form-style-5 label {
+	display: block;
+	margin-bottom: 8px;
+}
+.form-style-5 input[type="text"],
+.form-style-5 input[type="date"],
+.form-style-5 input[type="datetime"],
+.form-style-5 input[type="email"],
+.form-style-5 input[type="number"],
+.form-style-5 input[type="search"],
+.form-style-5 input[type="time"],
+.form-style-5 input[type="url"],
+.form-style-5 textarea,
+.form-style-5 select {
+	font-family: Georgia, "Times New Roman", Times, serif;
+	background: rgba(255,255,255,.1);
+	border: none;
+	border-radius: 4px;
+	font-size: 15px;
+	margin: 0;
+	outline: 0;
+	padding: 10px;
+	width: 100%;
+	box-sizing: border-box; 
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box; 
+	background-color: #e8eeef;
+	color:#8a97a0;
+	-webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+	box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+	margin-bottom: 30px;
+}
+.form-style-5 input[type="text"]:focus,
+.form-style-5 input[type="date"]:focus,
+.form-style-5 input[type="datetime"]:focus,
+.form-style-5 input[type="email"]:focus,
+.form-style-5 input[type="number"]:focus,
+.form-style-5 input[type="search"]:focus,
+.form-style-5 input[type="time"]:focus,
+.form-style-5 input[type="url"]:focus,
+.form-style-5 textarea:focus,
+.form-style-5 select:focus{
+	background: #d2d9dd;
+}
+.form-style-5 select{
+	-webkit-appearance: menulist-button;
+	height:35px;
+}
+.form-style-5 .number {
+	background: #1abc9c;
+	color: #fff;
+	height: 30px;
+	width: 30px;
+	display: inline-block;
+	font-size: 0.8em;
+	margin-right: 4px;
+	line-height: 30px;
+	text-align: center;
+	text-shadow: 0 1px 0 rgba(255,255,255,0.2);
+	border-radius: 15px 15px 15px 0px;
+}
 
-      body {
-        background-color: #eaedf4;
-        font-family: "Rubik", sans-serif;
-      }
-
-      .card {
-        width: 310px;
-        border: none;
-        border-radius: 15px;
-      }
-
-      .justify-content-around div {
-        border: none;
-        border-radius: 20px;
-        background: #f3f4f6;
-        padding: 5px 20px 5px;
-        color: #8d9297;
-      }
-
-      .justify-content-around span {
-        font-size: 12px;
-      }
-
-      .justify-content-around div:hover {
-        background: #545ebd;
-        color: #fff;
-        cursor: pointer;
-      }
-
-      .justify-content-around div:nth-child(1) {
-        background: #545ebd;
-        color: #fff;
-      }
-
-      span.mt-0 {
-        color: #8d9297;
-        font-size: 12px;
-      }
-
-      h6 {
-        font-size: 15px;
-      }
-      .mpesa {
-        background-color: green !important;
-      }
-
-      img {
-        border-radius: 15px;
-      }
-    </style>
+.form-style-5 input[type="submit"],
+.form-style-5 input[type="button"]
+{
+	position: relative;
+	display: block;
+	padding: 19px 39px 18px 39px;
+	color: #FFF;
+	margin: 0 auto;
+	background: #1abc9c;
+	font-size: 18px;
+	text-align: center;
+	font-style: normal;
+	width: 100%;
+	border: 1px solid #16a085;
+	border-width: 1px 1px 3px;
+	margin-bottom: 10px;
+}
+.form-style-5 input[type="submit"]:hover,
+.form-style-5 input[type="button"]:hover
+{
+	background: #109177;
+}
+</style>
 </head>
 
 <!-- Google tag (gtag.js) -->
@@ -132,37 +182,23 @@
                   
                     <h4></h4>
                 </div>
-                <div class="login">    
-                    <form class="row g-3" action="./stk_initiate.php" method="POST">
-            
-                      
-                        <div class="col-12">
-                          <label for="inputAddress2" class="form-label" >Lucky Numbers</label>
-                          <input type="text" class="form-control" name="luckynumbers"  placeholder="eg, 1, 10, 20, 30, 40" required>
-                        </div>
-        
-                        <div class="col-12">
-                          <label for="inputAddress2" class="form-label" >Game</label>
-                          <input type="text" class="form-control" name="game"  placeholder="Nuru" required>
-                        </div>
-
-                        <div class="col-12">
-                          <label for="inputAddress" class="form-label">Amount</label>
-                          <input type="text" class="form-control" name="amount" placeholder="Enter Amount" required>
-                        </div>
-
-                        <div class="col-12">
-                          <label for="inputAddress2" class="form-label" >Phone Number</label>
-                          <input type="text" class="form-control" name="phone"  placeholder="Enter Phone Number" required>
-                        </div>
-        
-                        <div class="col-12">
-                          <button type="submit" class="btn btn-success normal" name="submit" value="submit">Play</button>
-                        </div>
-                      </form>
-                </div>    
-
                
+   
+<div class="form-style-5">
+<form>
+<fieldset>
+<legend><span class="number"></span> Your Info</legend>
+<input type="text" name="field1" placeholder="Your Name *" required>
+<input type="email" name="field2" placeholder="Your Email *" required>
+<input type="text" class="form-control" name="luckynumbers"  placeholder="eg, 1, 10, 20, 30, 40" required>
+<input type="text" class="form-control" name="gametype"  placeholder="Nuru" required>
+<input type="text" class="form-control" name="phone"  placeholder="254xxxxxxxxx" required>
+
+
+</fieldset>
+<input type="submit" value="Play" />
+</form>
+</div>
                       
 
             </div>
@@ -175,34 +211,21 @@
                   
                    <h4 style="color: black ;">Play for a night with an exotic escort (White/Indian)</h4>
                 </div> 
-                <div class="login">    
-                <form class="row g-3" action="./stk_initiate.php" method="POST">
-                    
-            <div class="col-12">
-              <label for="inputAddress2" class="form-label" >Lucky Numbers</label>
-              <input type="text" class="form-control" name="luckynumbers"  placeholder="eg, 1, 10, 20, 30, 40" required>
-            </div>
+                <div class="form-style-5">
+<form>
+<fieldset>
+<legend><span class="number"></span> Your Info</legend>
+<input type="text" name="field1" placeholder="Your Name *" required>
+<input type="email" name="field2" placeholder="Your Email *" required>
+<input type="text" class="form-control" name="luckynumbers"  placeholder="eg, 1, 10, 20, 30, 40" required>
+<input type="text" class="form-control" name="gametype"  placeholder="Exotic" required>
+<input type="text" class="form-control" name="phone"  placeholder="254xxxxxxxxx" required>
 
-            <div class="col-12">
-              <label for="inputAddress2" class="form-label" >Game</label>
-              <input type="text" class="form-control" name="game"  placeholder="Exotic" required>
-            </div>
 
-            <div class="col-12">
-              <label for="inputAddress" class="form-label">Amount</label>
-              <input type="text" class="form-control" name="amount" placeholder="Enter Amount" required>
-            </div>
-
-            <div class="col-12">
-              <label for="inputAddress2" class="form-label" >Phone Number</label>
-              <input type="text" class="form-control" name="phone"  placeholder="Enter Phone Number" required>
-            </div>
-
-            <div class="col-12">
-              <button type="submit" class="btn btn-success normal" name="submit" value="submit">Play</button>
-            </div>
-          </form>
-    </div>    
+</fieldset>
+<input type="submit" value="Play" />
+</form>
+</div>
 
                 </div>    
 
